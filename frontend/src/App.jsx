c946ee3987de1b57db0e7923daf7822c1bf42f6e
jordanschneringer <148251614+jordanschneringer@users.noreply.github.com>
2023-11-02 12:49:40 -0400

    set up environment
    added open ai keys

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import {useState} from 'react';
+import {Configuation, OpenAIApi} from 'openai';
+
+const configuation = new Configuation ({
+  organization: "org-KnkH7hmgngd8QoQWyTpOZydU",
+  apiKey: "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"
+})
